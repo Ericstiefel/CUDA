a5b20ce4ed26b750585ee5168d1fc61e553d1845
Eric <Eric.Stiefel@gmail.co>
2026-07-26 15:40:16 -0400

    adding full gemm kernel

--- GPT2/gemm.cu
+++ GPT2/gemm.cu
@@ -6,7 +6,7 @@ __device__ __forceinline__ uint32_t smem_ptr_to_uint(const void* ptr) {
     return static_cast<uint32_t>(__cvta_generic_to_shared(ptr));
 }
 
-__device__ __forceinline__ uint32_t swizzle_address(const half* base_smem, int row, int col, int stride) {
+__device__ __forceinline__ uint32_t swizzle_addr(const half* base_smem, int row, int col, int stride) {
     int vals_per_vec = 16 / sizeof(half); // 8 2 byte half values fit inside 1 transfer 16 byte load
     int vecs_per_row = stride / vals_per_vec;
 
@@ -87,7 +87,138 @@ __global__ void gemm(const half* __restrict__ A, const half* __restrict__ B, flo
     // A has width 32, so we're loading 8 eles at a time, 32 / 8 = 4.
     int load_A_row = blockIdx.y * BM + (tid / 4);
     int load_A_col = (tid % 4) * 8;
+    
+    // B tile has width 128, so we use 16 here
+    int load_B_row = tid / 16;
+    int load_B_col = blockIdx.x * BN + (tid % 16) * 8;
 
+    uint32_t rA[4]; uint32_t rB[2];
+    float rC[4][4][4] = {0.0f};
+
+    int write_stage = 0; int read_stage = 0;
+
+    uint32_t smem_A_p1 = swizzle_addr(&sA[0][0][0], tid / 4, (tid / 4) * 8, BK);
+    uint32_t smem_A_p2 = swizzle_addr(&sA[0][0][0], tid / 4 + 64, (tid / 4) * 8, BK);
+    cp_async_128(smem_A_p1, &A[load_A_row * K + load_A_col]);
+    cp_async_128(smem_A_p2, &A[(load_A_row + 64) * K + load_A_col]);
+
+    uint32_t smem_B_p1 = swizzle_addr(&sB[0][0][0], tid / 16, (tid % 16) * 8, BN);
+    uint32_t smem_B_p2 = swizzle_addr(&sB[0][0][0], tid / 16 + 16, (tid % 16) * 8, BN);
+    cp_async_128(smem_B_p1, &B[load_B_row * N + load_B_col]);
+    cp_async_128(smem_B_p2, &B[(load_B_row + 16) * N + load_B_col]);
+
+    cp_async_commit_group(); cp_async_wait_group<0>(); __syncthreads();
+
+    int warp_row = warp_id / 4; int warp_col = warp_id % 4; // where that warp is inside of the 2x4 warp grid.
+
+    int warp_row_offset = warp_row * 64; int warp_col_offset = warp_col * 32;
+
+    for (int tile_k = BK; tile_k < K; tile_k += BK) {
+        // preload next set of tiles
+        write_stage ^= 1;
+
+        uint32_t smem_A_p1 = swizzle_addr(&sA[write_stage][0][0], tid / 4, (tid / 4) * 8, BK);
+        uint32_t smem_A_p2 = swizzle_addr(&sA[write_stage][0][0], tid / 4 + 64, (tid / 4) * 8, BK);
+        cp_async_128(smem_A_p1, &A[load_A_row * K + load_A_col + tile_k]);
+        cp_async_128(smem_A_p2, &A[(load_A_row + 64) * K + load_A_col + tile_k]);
+
+        uint32_t smem_B_p1 = swizzle_addr(&sB[write_stage][0][0], tid / 16, (tid % 16) * 8, BN);
+        uint32_t smem_B_p2 = swizzle_addr(&sB[write_stage][0][0], tid / 16 + 16, (tid % 16) * 8, BN);
+        cp_async_128(smem_B_p1, &B[load_B_row * N + load_B_col + tile_k * N]);
+        cp_async_128(smem_B_p2, &B[(load_B_row + 16) * N + load_B_col + tile_k * N]);
+
+        cp_async_commit_group(); 
+
+        for (int k_dim = 0; k_dim < 2; ++k_dim) {
+            for (int m_dim = 0; m_dim < 4; ++m_dim) {
+                int lane_group = lane / 8; 
+                int row_in_group = lane % 8;
+                
+
+                // tensor core ldmatrix register pattern has it as 4 8x8 tiles
+                int a_row, a_col;
+                a_row = warp_row_offset + m_dim * 16 + row_in_group;
+                a_row = (lane_group == 0 || lane_group == 2) ? a_row : a_row + 8;
+
+                a_col = (lane_group == 0 || lane_group == 1) ? k_dim * 16 : k_dim * 16 + 8;
+
+                uint32_t smem_read_A = swizzle_addr(&sA[read_stage][0][0], a_row, a_col, BK);
+                ld_matrix_x4(rA, smem_read_A);
+
+                for (int n_dim = 0; n_dim < 4; ++n_dim) {
+                    int b_row, b_col;
+                    b_row = k_dim * 16 + row_in_group;
+                    b_row = (lane_group == 0 || lane_group == 2) ? b_row : b_row + 8;
+
+                    b_col = warp_col_offset + n_dim * 8;
+
+                    uint32_t smem_read_B = swizzle_addr(&sB[read_stage][0][0], b_row, b_col, BN);
+                    ld_matrix_x2(rB, smem_read_B);
+                    mma(rA, rB, rC[m_dim][n_dim]);
+                }
+            }
+        }
+
+        read_stage ^= 1;
+        wait_group<1>();
+        __syncthreads();
+
+    }
+
+    // last tile
+    for (int k_dim = 0; k_dim < 2; ++k_dim) {
+        for (int m_dim = 0; m_dim < 4; ++m_dim) {
+            int lane_group = lane / 8; 
+            int row_in_group = lane % 8;
+            
+
+            // tensor core ldmatrix register pattern has it as 4 8x8 tiles
+            int a_row, a_col;
+            a_row = warp_row_offset + m_dim * 16 + row_in_group;
+            a_row = (lane_group == 0 || lane_group == 2) ? a_row : a_row + 8;
+
+            a_col = (lane_group == 0 || lane_group == 1) ? k_dim * 16 : k_dim * 16 + 8;
+
+            uint32_t smem_read_A = swizzle_addr(&sA[read_stage][0][0], a_row, a_col, BK);
+            ld_matrix_x4(rA, smem_read_A);
+
+            for (int n_dim = 0; n_dim < 4; ++n_dim) {
+                int b_row, b_col;
+                b_row = k_dim * 16 + row_in_group;
+                b_row = (lane_group == 0 || lane_group == 2) ? b_row : b_row + 8;
+
+                b_col = warp_col_offset + n_dim * 8;
+
+                uint32_t smem_read_B = swizzle_addr(&sB[read_stage][0][0], b_row, b_col, BN);
+                ld_matrix_x2(rB, smem_read_B);
+                mma(rA, rB, rC[m_dim][n_dim]);
+            }
+        }
+    }
+
+    // output is 16 x 8 per tile, each thread has 2 consecutive items in each of its 2 output rowws.
+    int frag_row = lane / 4;
+    int frag_col = (lane % 4) * 2;
+
+    int warp_global_row = blockIdx.y * BM + warp_row_offset;
+    int warp_global_col = blockIdx.x * BN + warp_col_offseet;
+
+    #pragma unroll
+    for (int m = 0; m < 4; ++m) {
+        #pragma unroll
+        for (int n = 0; n < 4; ++n) {
+            int block_global_row = warp_global_row + (m * 16);
+            int block_global_col = warp_global_col + (n * 8);
+
+            int global_row = block_global_row + frag_row;
+            int global_col = block_global_col + frag_col;
+
+            C[global_row * N + global_col] = rC[m][n][0];
+            C[global_row * N + global_col + 1] = rC[m][n][1];
+            C[(global_row + 8) * N + global_col] = rC[m][n][2];
+            C[(global_row + 8) * N + global_col + 1] = rC[m][n][3];
+        }
+    }
 
 
 }
